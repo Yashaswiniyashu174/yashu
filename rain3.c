007c077c89b9de4dc729ec27e32829b1c5e1b72b
Yashaswiniyashu174 <vvce22ec0203@vvce.ac.in>
2025-03-08 10:16:15 +0530

    Add files via upload

---
+++ rain3.c
@@ -0,0 +1,16 @@
+
+//UPDATE
+
+#include <stdio.h>
+int main(){
+FILE *file = fopen("example.txt", "a+");
+if (file== NULL) {
+printf("Error opening file for writing.\n");
+return 1; }
+fseek(file,0,SEEK_SET);
+fprintf(file, "Updated content: Hello, updated World!\n");
+fclose(file);
+printf("Data updated to file successfully!\n");
+return 0; }
+
+
